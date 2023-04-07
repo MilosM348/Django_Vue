b3d9fa2f935f632178917d0fbbf693c5c8e1d443
MilosM348 <milos.milic001@outlook.com>
2023-04-07 07:26:11 +0200

    fix view

--- backend/todo/api/views.py
+++ backend/todo/api/views.py
@@ -4,6 +4,9 @@ from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser 
 from rest_framework import status
  
+from api.models import Item
+from api.serializers import ItemSerializer
+from rest_framework.decorators import api_view
 
 
 @api_view(['GET', 'POST', 'DELETE'])
@@ -19,18 +22,7 @@ def item_list(request):
         items_serializer = ItemSerializer(items, many=True)
         return JsonResponse(items_serializer.data, safe=False)
         # 'safe=False' for objects serialization
- 
-    elif request.method == 'POST':
-        print('item post')
-        item_data = JSONParser().parse(request)
-        print(item_data)
-        item_data['id'] = -1
-        item_serializer = ItemSerializer(data=item_data)
-        if item_serializer.is_valid():
-            item_serializer.save()
-            return JsonResponse(item_serializer.data, status=status.HTTP_201_CREATED) 
-        return JsonResponse(item_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
     elif request.method == 'DELETE':
         count = Item.objects.all().delete()
         return JsonResponse({'message': '{} Items were deleted successfully!'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
